652325dd63e717e424b44de3969efed15710b4e8
Cynthion21x <cynthion21x@gmail.com>
2024-10-13 22:01:42 +0100

    ill start properly later.

--- main.cc
+++ main.cc
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <SDL2/SDL.h>
+#include "core.h"
 
 int main(int argsc, char* argsv[]) {
 
-    std::cout << "Hello world" << std::endl;
+    if(SDL_Init(SDL_INIT_VIDEO) < 0 ) {
+        std::cout << "SDL could not initialize! SDL_Error: %s\n" << SDL_GetError() << std::endl;
+    }
 
     return 0;
 
